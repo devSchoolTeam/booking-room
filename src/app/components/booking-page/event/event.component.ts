bd94cd7351a2e682682f053203fb3f3ed784ae37
Chizz <mgandrey99@gmail.com>
2018-09-21 16:55:37 +0300

    change align of blocks, fonts and padding inside of blocks

--- src/app/components/booking-page/event/event.component.ts
+++ src/app/components/booking-page/event/event.component.ts
@@ -19,13 +19,11 @@ export class EventComponent implements OnInit {
       next: events => {
         this.events = events;
         for (let i = 0; i < this.events.length; i++) {
-          console.log(events[i]);
           const startTime = new Date(events[i].start.dateTime);
           const endTime = new Date(events[i].end.dateTime);
           this.eventDuration = startTime.toLocaleTimeString().slice(0, 5) + '-' + endTime.toLocaleTimeString().slice(0, 5);
           this.eventDurations.push(this.eventDuration);
         }
-        console.log(this.eventDurations);
       }
     });
   }
