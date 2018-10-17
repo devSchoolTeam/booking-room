a419b2144cdd0a52ce738c2889bb1b72c94990f7
DanilDidenko <danilkadidenko@gmail.com>
2018-10-17 19:23:21 +0300

    Workin config has been changed

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,7 +1,7 @@
 export const clientConfig = {
   clientId:
-    '1021277222775-3k2hkvmlbbh2sd8cok5ps4uin4nbsoj3.apps.googleusercontent.com',
-  apiKey: 'AIzaSyCX9rlRKtTdVnl7hmOxfeIZkNreGa1xJ3g',
+    '881677375382-o3a5d10j96iu7j963ah0q2kqs560lgol.apps.googleusercontent.com',
+  apiKey: 'AIzaSyC5sAcA8u1q6uiK2A-fc6m3gfcg3aaoiuI',
   discoveryDocs: [
     `https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest`
   ],
